852b09bea708548aa1328d03e4dbe269765796e5
DGUHJH <ghkdwogud852@gmail.com>
2023-05-13 15:19:19 +0900

    feat : ProjectCard randomLogoImg 기능 추가

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,6 @@
-import sampleCardImg1 from 'assets/images/card/card_img1.png'
 import { FC } from 'react'
 import { ProjectItemType } from 'types/project'
+import { generateRandomProjectCardLogoImg } from 'utils/generateRandomProjectCardLogoImg'
 import { getDevelopmentStackColor, translateDevelopmentStack } from 'utils/translateDevelopmentStack'
 import { CardMeta, DevelopmentStackTag, DevelopmentStackTagContainer, RepresentativeImg, Root } from './styled'
 
@@ -14,7 +14,12 @@ export const ProjectCard: FC<ProjectCardProps> = ({ className, projectData }) =>
     <Root
       className={className}
       hoverable
-      cover={<RepresentativeImg src={projectData.representativeImg ?? sampleCardImg1} alt={'프로젝트 대표 이미지'} />}
+      cover={
+        <RepresentativeImg
+          src={projectData.representativeImg ?? generateRandomProjectCardLogoImg()}
+          alt={'프로젝트 대표 이미지'}
+        />
+      }
     >
       <CardMeta title={projectData.title} description={`조회수 : ${projectData.visitedNumber}회`} />
       <DevelopmentStackTagContainer size={[0, 3]} wrap>
