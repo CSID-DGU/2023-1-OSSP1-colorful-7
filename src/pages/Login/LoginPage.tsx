9ced2a0f3a9b924aa1d42279e5cbe83dadc1aafd
DGUHJH <ghkdwogud852@gmail.com>
2023-05-13 15:37:03 +0900

    style : LoginPage 레이아웃 구성

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,10 +1,49 @@
+import logoImg from 'assets/images/logo.png'
+import { CommonHeader } from 'components/CommonHeader'
 import { FC } from 'react'
-import { Root } from './styled'
+import { useNavigate } from 'react-router-dom'
+import {
+  Container,
+  ContentInput,
+  InputContainer,
+  JoinButton,
+  JoinContainer,
+  JoinInfoTypo,
+  LoginButton,
+  LogoImg,
+  LogoTypo,
+  Root,
+} from './styled'
 
 type LoginPageProps = {
   className?: string
 }
 
 export const LoginPage: FC<LoginPageProps> = ({ className }) => {
-  return <Root className={className}>LoginPage</Root>
+  const navigate = useNavigate()
+
+  const onClickJoinButton = () => {
+    navigate('/join')
+  }
+
+  return (
+    <Root className={className}>
+      <CommonHeader />
+      <Container>
+        <LogoImg src={logoImg} alt={'로고 이미지'} />
+        <LogoTypo>당신의 능력, 티밍에서 펼쳐보세요!</LogoTypo>
+        <InputContainer>
+          <ContentInput placeholder="아이디" />
+          <ContentInput placeholder="비밀번호" type="password" />
+        </InputContainer>
+        <LoginButton type="primary">로그인</LoginButton>
+        <JoinContainer>
+          <JoinInfoTypo>아직 회원이 아니세요?</JoinInfoTypo>
+          <JoinButton type="text" onClick={onClickJoinButton}>
+            회원가입
+          </JoinButton>
+        </JoinContainer>
+      </Container>
+    </Root>
+  )
 }
