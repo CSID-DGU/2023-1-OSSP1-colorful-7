f7f76e42820aa2661247fb43dd6123f33e2358fe
DGUHJH <ghkdwogud852@gmail.com>
2023-05-13 12:38:50 +0900

    feat : questionnaire type 추가

---
+++ src/types/questionnaire.ts
@@ -0,0 +1,19 @@
+export type QuestionType = 'RADIO' | 'SELECT'
+
+export type QuestionDataType = {
+  key: number
+  type: QuestionType
+  title: string
+  answerKeyList: number[]
+  score: number
+  question: {
+    key: number
+    title: string
+  }[]
+}
+
+export type QuestionnaireDataType = {
+  title: string
+  questionnaireType: string
+  questionListData: QuestionDataType[]
+}
