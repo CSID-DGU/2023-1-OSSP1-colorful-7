51c40f683caa182dd2670da66be4b61bda0ee798
DGUHJH <ghkdwogud852@gmail.com>
2023-05-15 17:11:04 +0900

    feat : ProjectListPage 레이아웃 구성

--- src/pages/Project/List/ProjectListPage.tsx
+++ src/pages/Project/List/ProjectListPage.tsx
@@ -1,10 +1,27 @@
+import { CommonHeader } from 'components/CommonHeader'
+import { ProjectCard } from 'components/ProjectCard'
+import projectListSampleJson from 'constants/json/project_list_sample.json'
 import { FC } from 'react'
-import { Root } from './styled'
+import { ProjectListType } from 'types/project'
+import { camelizeKey } from 'utils/camelizeKey'
+import { ProjectCardContainer, Root } from './styled'
 
 type ProjectListPageProps = {
   className?: string
 }
 
 export const ProjectListPage: FC<ProjectListPageProps> = ({ className }) => {
-  return <Root className={className}>ProjectListPage</Root>
+  const projectListData = camelizeKey(projectListSampleJson.project_list) as ProjectListType
+  return (
+    <Root className={className}>
+      <CommonHeader />
+      <ProjectCardContainer>
+        {projectListData
+          .sort((a, b) => a.key - b.key)
+          .map((projectItemData) => (
+            <ProjectCard projectData={projectItemData} key={`project_card_${projectItemData.key}`} />
+          ))}
+      </ProjectCardContainer>
+    </Root>
+  )
 }
--- src/pages/Project/List/styled.ts
+++ src/pages/Project/List/styled.ts
@@ -1,3 +1,22 @@
+import { CONTAINER_WIDTH, HEADER_HEIGHT } from 'constants/system/layout'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: ${HEADER_HEIGHT}px;
+  position: relative;
+  padding-bottom: 80px;
+`
+
+export const ProjectCardContainer = styled.div`
+  width: ${CONTAINER_WIDTH}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-top: 40px;
+`
