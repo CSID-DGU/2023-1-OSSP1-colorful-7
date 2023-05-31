f6dc2b9009d3a0742c0bcf84b474bde5cedfa993
SeungJi20 <lina6709@naver.com>
2023-05-31 15:16:30 +0900

    db구축

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping()
     public Hello get() {
-        return new Hello(0, "Hello Spring Boot!");
+        return new Hello(0, "Hello Spring Boot! Hi");
     }
 }
