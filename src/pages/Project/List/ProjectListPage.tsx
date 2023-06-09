3d756ec5ccf129b1b3675b991e4c4439c617a68e
chungchae <chungchae0@gmail.com>
2023-06-10 00:29:22 +0900

    필터 레이아웃 제작

--- src/pages/Project/List/ProjectListPage.tsx
+++ src/pages/Project/List/ProjectListPage.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable  */
 import { CommonHeader } from 'components/CommonHeader';
 import { ProjectCard } from 'components/ProjectCard';
 import projectListSampleJson from 'constants/json/project_list_sample.json';
 import React, { FC, useState } from 'react';
-import { ProjectListType } from 'types/project';
+import { ProjectListType, ProjectType } from 'types/project';
 import { camelizeKey } from 'utils/camelizeKey';
-import { ProjectCardContainer, Root, SearchBox, SearchContainer } from './styled';
+import { ProjectCardContainer, Root, Container, SelectContainer, SearchBox, SearchContainer, SelectBox } from './styled';
 
 type ProjectListPageProps = {
   className?: string;
@@ -12,12 +13,26 @@ type ProjectListPageProps = {
 
 const projectListData = camelizeKey(projectListSampleJson.project_list) as ProjectListType;
 
-export const ProjectListPage: FC<ProjectListPageProps> = ({ className }) => {
-  const [search, setSearch] = useState('');
+type OptionType = {
+  value: string;
+  label: string;
+};
 
-  const filteredProjectListData = projectListData.filter((projectItem) =>
-    projectItem.title.toLowerCase().includes(search.toLowerCase())
-  );
+const options: OptionType[] = [
+  { value: "web", label: "WEB" },
+  { value: "app", label: "APP" },
+  { value: "etc", label: "ETC" },
+];
+
+export const ProjectListPage: FC<ProjectListPageProps> = ({ className }) => {
+  const [search, setSearch] = useState('')
+  const [select, setSelect] = useState('')
+
+  const filteredProjectListData = projectListData.filter(
+    (projectItem) =>
+      projectItem.title.toLowerCase().includes(search.toLowerCase()) &&
+      projectItem.projectType.toLowerCase().includes(select.toLowerCase())
+  )
 
   const renderProjectList = () => {
     return (
@@ -28,28 +43,44 @@ export const ProjectListPage: FC<ProjectListPageProps> = ({ className }) => {
             <ProjectCard projectItem={projectItem} key={`project_card_${projectItem.key}`} />
           ))}
       </ProjectCardContainer>
-    );
-  };
-  
-  //@ts-ignore
+    )
+  }
+
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.currentTarget.value);
-  };  
+    setSearch(event.currentTarget.value)
+  }
+  
+  const handleSelectChange = (option: ValueType<OptionType>) => {
+    setSelect(option);
+  }
 
   return (
     <Root className={className}>
       <CommonHeader />
-      <SearchContainer>
-        <SearchBox
-          type="search"
-          id="psearch"
-          name="psearch"
-          maxLength={20}
-          placeholder="제목 검색"
-          onChange={handleSearchChange}
-        />
-      </SearchContainer>
+      <Container>
+        <SelectContainer>
+          <SelectBox
+            onChange={(option) => handleSelectChange(option)}
+            defaultValue="web"
+            options={[
+              { value: 'web', label: 'WEB' },
+              { value: 'app', label: 'APP' },
+              { value: 'etc', label: 'ETC' },
+            ]}
+          />
+        </SelectContainer>
+        <SearchContainer>
+          <SearchBox
+            type="search"
+            id="psearch"
+            name="psearch"
+            maxLength={20}
+            placeholder="제목 검색"
+            onChange={handleSearchChange}
+          />
+        </SearchContainer>
+      </Container>
       {renderProjectList()}
     </Root>
-  );
-};
+  )
+}
--- src/pages/Project/List/styled.ts
+++ src/pages/Project/List/styled.ts
@@ -1,4 +1,4 @@
-import { Input } from 'antd'
+import { Input, Select } from 'antd'
 import { CONTAINER_WIDTH, HEADER_HEIGHT } from 'constants/system/layout'
 import styled from 'styled-components'
 
@@ -21,6 +21,20 @@ export const ProjectCardContainer = styled.div`
   gap: 10px;
   margin-top: 40px;
 `
+export const Container = styled.div`
+  width: ${CONTAINER_WIDTH}px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
+`
+export const SelectContainer = styled.div`
+  width: ${CONTAINER_WIDTH}px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+`
 
 export const SearchContainer = styled.div`
   width: ${CONTAINER_WIDTH}px;
@@ -29,8 +43,10 @@ export const SearchContainer = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   gap: 10px;
-  margin-top: 40px;
+  margin-top: 20px;
 `
 
 export const SearchBox = styled(Input)`
+`
+export const SelectBox = styled(Select)`
 `
